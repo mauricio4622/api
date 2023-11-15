0f4eac86e8f94bcfc265dfdb187f2cdab15c4862
mauricio4622 <147119073+mauricio4622@users.noreply.github.com>
2023-11-15 18:56:36 -0500

    trabajo

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+    // Get the submitted data
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmPassword = $_POST['confirm-password'];
+
+    // Perform validation checks here (e.g., check if the passwords match)
+    // If valid, proceed with the signup process
+    // If invalid, redirect back to the signup page with an error message
+
+    // Insert the new user into the database
+
+    // Redirect the user to their dashboard
+
+?>
